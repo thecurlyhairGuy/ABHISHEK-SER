e626f292d4e20bbda3895ad36c5fe02cc664dc0a
CurlyhairGuy <149983691+thecurlyhairGuy@users.noreply.github.com>
2024-01-07 19:44:52 -0800

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ global.owner = [
   [''] 
 ] //Number of owners
 
-global.mods = ['2348143181595','2349115175330'] 
-global.prems = ['2348143181595', '2349115175330',]
+global.mods = ['2348143181595','2349115175330','2347014272228'] 
+global.prems = ['2348143181595', '2349115175330','2347014272228']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
